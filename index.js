350fc996a7312c8cfc327d76d02bf5be27d177aa
malanshan314159 <124501895+malanshan314159@users.noreply.github.com>
2024-03-14 23:16:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let i = 10; console.log(i);
+var add=x=>(y=>x+y);
+var add2 = add(2);
+console.log(add2);
+console.log(add2(3));
+var msg = "ab,cd,efg,hijk";
+console.log(msg.split(",").reverse().join(''));
